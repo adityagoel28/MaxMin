3fee8ec3af783859c97f101919964dcf0412a004
adityagoel28 <adityagoel2002@gmail.com>
2023-12-27 00:10:47 +0530

    Float Comparison

--- src/main/java/MaxMin/Max_Min.java
+++ src/main/java/MaxMin/Max_Min.java
@@ -13,4 +13,18 @@ public class Max_Min {
 
         return max;
     }
+
+    public static Double findMax(Double x, Double y, Double z) {
+        Double max = x; // Assume x is initially the largest
+
+        if (y.compareTo(max) > 0) {
+            max = y; // y is larger
+        }
+
+        if (z.compareTo(max) > 0) {
+            max = z; // z is larger
+        }
+
+        return max;
+    }
 }
