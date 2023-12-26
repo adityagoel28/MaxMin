81c3b7f8ef309e1f0e8e727a8d70f0e3e5f54d7e
adityagoel28 <adityagoel2002@gmail.com>
2023-12-27 00:09:36 +0530

    Integer Comparison

---
+++ src/main/java/MaxMin/Max_Min.java
@@ -0,0 +1,16 @@
+package MaxMin;
+
+public class Max_Min {
+    public static Integer findMax(Integer x, Integer y, Integer z) {
+        Integer max = x; // Assume x is initially the largest
+
+        if (y.compareTo(max) > 0) {
+            max = y; // y is larger
+        }
+        if (z.compareTo(max) > 0) {
+            max = z; // z is larger
+        }
+
+        return max;
+    }
+}
